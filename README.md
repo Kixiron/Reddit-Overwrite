b0336f5372129f03ebfe1a7d9791e354280dbaa4
Kixiron <25047011+Kixiron@users.noreply.github.com>
2019-01-05 13:14:21 -0600

    Final script version

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Reddit-Overwrite
-Overwrites reddit comments
+
+Overwrites reddit comments using Selenium.
+
+To install Selenium, follow this [handy guide](https://selenium-python.readthedocs.io/installation.html)  
+When running this, you'll get lots of handy little automoderator messages in roughly this format:
+
+```txt
+Greetings, /u/USERNAME! Your comment has been automatically removed because it is spam. Spamming is a really shitty thing to do. This script is dumb and pointless. You actually aren't even protecting any privacy because there are many sites out there that specifically cache comments just so that users cannot edit them.
+
+To reiterate, this script is terrible and you should not be using it. Search for a different one, or edit it to say something less spammy. But in the end, it won't matter because we can still see whatever it was that you edited.
+
+You should also read this: https://reddit.zendesk.com/hc/en-us/articles/204536499-What-constitutes-spam-Am-I-a-spammer-
+
+Did I mention that is spammy and we don't like it?
+
+I am a bot, and this action was performed automatically. Please contact the moderators of this subreddit if you have any questions or concerns.
+```
+
+As far as I can tell, these do nothing.
+
+Disclaimer: Any and all potential consequences from running this script are 100% yours.
--- src/Overwrite.py
+++ src/Overwrite.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 MESSAGE = "This comment has been overwritten by an open source script to protect this user's privacy.  \nIf you would like to do the same, simply run [this python script](https://github.com/Kixiron/Reddit-Overwrite)"
 
 def login():
+    print('Please input login info for Reddit (This is not stored or transmitted)')
     USERNAME = input('Username: ')
     PASSWORD = input('Password: ')
     print('Logging in...')
@@ -13,7 +14,8 @@ def login():
     driver.find_element_by_id('loginPassword').send_keys(PASSWORD)
     driver.find_element_by_class_name('AnimatedForm__submitButton').click()
     driver.get('https://old.reddit.com/user/{}/comments'.format(USERNAME))
-    input('Make sure you\'re logged in to the proper reddit account [Press Enter to continue]')
+    input('Make sure you\'re logged in to the proper reddit account and then press Enter to continue')
+    return USERNAME
 
 def delete_page_comments():
     comment_num = 0
@@ -30,18 +32,42 @@ def delete_page_comments():
             if edit_button != None and delete_button != None:
                 break
         edit_button.click()
-        textarea = comment.find_element_by_class_name('MarkdownForm__text')
-        textarea.clear()
-        textarea.send_keys(MESSAGE)
-        comment.find_element_by_class_name('MarkdownForm__submit').click()
-        delete_button.click()
-        comment.find_element_by_class_name('RestrictedFlatlistButton__toggleOption').click()
-        print('Deleted comment {}/{}'.format(comment_num, len(comments)))
+        try:
+            textarea = comment.find_element_by_class_name('MarkdownForm__text')
+            textarea.clear()
+            textarea.send_keys(MESSAGE)
+            comment.find_element_by_class_name('MarkdownForm__submit').click()
+            delete_button.click()
+            comment.find_element_by_class_name('RestrictedFlatlistButton__toggleOption').click()
+            print('Deleted comment')
+        except:
+            print('Failed to delete comment')
+            pass
+    return comment_num
 
 driver = webdriver.Firefox()
-login()
-print('Locating comments...')
-while driver.find_element_by_class_name('ListingPagination__navButton') != None:
-    comments = driver.find_elements_by_class_name('CommentListing__comment')
-    delete_page_comments()
-    driver.find_element_by_class_name('ListingPagination__navButton').click()
+USERNAME = login()
+print('Are you sure you want to delete all comments from your profile?')
+confirmation = input('This is irrevocable and the script writer withdraws all responsibility for anything that happens\n')
+if confirmation.lower() != 'yes' or confirmation.lower() != 'y':
+    print('Deleting all comments will commence!')
+    print('Note that this may take a while and might sometimes require you to re-run the script because of Reddit ratelimiting')
+    print('While the script is running, try not to mess with the window opened by it')
+    comment_num = 0
+    while 1:
+        driver.get('https://old.reddit.com/user/{}/comments'.format(USERNAME))
+        print('Loaded new page of comments, deleting...')
+        comments = driver.find_elements_by_class_name('CommentListing__comment')
+        if len(comments) < 1:
+            print('All comments have been deleted from your profile!')
+            print('I deleted {} total comments'.format(comment_num))
+            print('It is recommended that you go through what remains of your comment history to check if all comments were deleted properly')
+            print('If something went wrong, please open an issue here: https://github.com/Kixiron/Reddit-Overwrite/issues/new')
+            input('Press Enter to Exit')
+            exit()
+        else:
+            comment_num += delete_page_comments()
+else:
+    print('Aborting...')
+    exit()
+
